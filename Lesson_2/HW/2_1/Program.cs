756466c82cc202e858d188a061993bc1277ca89a
celestialmary <mail@mail.ru>
2023-12-05 18:35:28 +0300

    Task 2_1 HW

---
+++ Lesson_2/HW/2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и проверяет, кратно ли оно
+//    одновременно 7 и 23
+
+void OneAndTwo(int num)
+{       
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine($"{num} -> yes");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> no");        
+    }    
+}
+
+int result = int.Parse(Console.ReadLine()!);
+OneAndTwo(result);
