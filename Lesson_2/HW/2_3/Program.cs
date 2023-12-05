c4a7f340c5b794f65cf454e3c3ddb4e6523a938c
celestialmary <mail@mail.ru>
2023-12-05 18:36:54 +0300

    Task 2_3 HW

---
+++ Lesson_2/HW/2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. Напишите программу, которая принимает
+//    на вход целое число из отрезка [10, 99] и показывает
+//    наибольшую цифру числа.
+
+int MaxNumber(int a)
+{
+    Console.WriteLine(a);
+    
+    if (a % 10 > a / 10)    
+        return a % 10;
+    return a / 10;
+    
+}
+
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
